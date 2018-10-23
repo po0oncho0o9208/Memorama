a36e1235b52eb819cc5f91aa92dfea029680d0f6
omarfhv <omar.09213@gmail.com>
2018-10-23 17:49:42 -0500

    boton salir y imagenes
    se arreglo botones

--- app/src/main/java/com/toposdeus/memorama/Principal.java
+++ app/src/main/java/com/toposdeus/memorama/Principal.java
@@ -1,15 +1,24 @@
 package com.toposdeus.memorama;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
 
 
     ImageView play,ajustes,trofeos;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +29,8 @@ public class Principal extends AppCompatActivity {
         ajustes = findViewById(R.id.btnajustes);
         trofeos = findViewById(R.id.btntrofeos);
 
+
+
     }
 
     public void play(View view) {
@@ -28,5 +39,60 @@ public class Principal extends AppCompatActivity {
         // Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://rh.imss.gob.mx/tarjetondigital/"));
         // startActivity(intent1);
     }
+
+    public void ajustes(View view) {
+        Intent intent12 = new Intent(this, Ajustes.class);
+        startActivity(intent12);
+        // Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://rh.imss.gob.mx/tarjetondigital/"));
+        // startActivity(intent1);
+    }
+
+    public void trofeos(View view) {
+        Intent intent13 = new Intent(this, Trofeos.class);
+        startActivity(intent13);
+        // Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://rh.imss.gob.mx/tarjetondigital/"));
+        // startActivity(intent1);
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+            View vi = inflater.inflate(R.layout.dialogoconfirm, null);
+            builder.setView(vi);
+            final AlertDialog dialog = builder.create();
+            //decidir despues si sera cancelable o no
+            dialog.setCancelable(false);
+            Button botonsi = vi.findViewById(R.id.botonsi);
+
+            botonsi.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.cancel();
+                            Principal.super.onDestroy();
+                            System.exit(0);
+                        }
+                    }
+            );
+            Button botonno = vi.findViewById(R.id.botonno);
+            botonno.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.cancel();
+
+                        }
+                    }
+            );
+            dialog.show();
+            //Metodos.dialogo( this, getLayoutInflater(), "¿seguro deseas salir de la aplicacion?", 0 );
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 
 }
---
+++ app/src/main/java/com/toposdeus/memorama/Trofeos.java
@@ -0,0 +1,13 @@
+package com.toposdeus.memorama;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Trofeos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trofeos);
+    }
+}
